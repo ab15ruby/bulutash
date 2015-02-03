99173b0525d7e7bbbcf99e896011b1dfd06a6d08
Mustafa Tekkoyun <mustafa.tekkoyun@gmail.com>
2015-02-03 12:38:22 +0200

    bulutash silindi.

---
+++ Kimlik.rb
@@ -0,0 +1,19 @@
+require 'date'
+module Kimlik
+	def Giris(kullanici,parola)
+		dosya=File.open("veritabani.txt","r").readlines().each do |satir|
+			parca=satir.split(",")
+			if (parca[0]==kullanici && parca[1]==parola)
+				if (Date.parse(parca[2])-Date.today >0)
+					return 1
+				else
+					return 2
+				end
+			end
+		end
+		return 3
+	end
+end
+class Test
+	include Kimlik
+end
