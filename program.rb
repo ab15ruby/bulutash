93d49624d0867654b6a288d171634840b5dc0d9d
ozgurefe <ozgrefe@gmail.com>
2015-02-05 00:50:55 +0200

    kullanıcı girişi bağlandı

--- program.rb
+++ program.rb
@@ -10,27 +10,48 @@ puts "yeni"
 end
 
 loop do
-	puts "MENÜ"
+	puts "Menü-1"
 	puts "1.Giriş"
 	puts "2.Yeni kullanıcı"
 	puts "3.Çıkış"
 	puts "Lutfen gir"
 	girdi=gets.chomp.to_i
 	
-	if    girdi==1 
+	if    girdi==1
 		loop do
-                        puts "Cok Yakinda"
-                        sleep(1.0)
-                        system("clear")
-                        sleep(1.0)
+		puts "Lütfen kullanıcı adınızı giriniz: "
+		kullanici_adi = gets.chomp.to_s
+		puts "Lütfen parolanizi giriniz: "
+		parola = gets.chomp.to_s
+		nesne = Kimlik.new
+		kontrol = nesne.giris(kullanici_adi,parola)
+			if kontrol == 3 
+				puts "Kullanici adınız veya parolaniz hatalı!!"
+				
+			elsif kontrol == 2
+				puts "Kullanim süreniz doldu!!"
+				break
+			else
+				loop do
+					puts "Menü-2"
+					puts "1.Dosya yükle"
+					puts "2.Dosya sil"
+					puts "3.Kullanım istatistiği"
+					puts "4.Dosyalarımı listele"
+					puts "5.Menü-1'e gitmek için"
+					puts "Yapmak istediğiniz işlemin numarasını giriniz: "
+					islem = gets.chomp.to_i
+					if islem == 1
+						puts "Yüklemek istediğiniz dosyanın adresini giriniz"
+						adres = gets.chomp.to_s
+						
+									
+				end
+				break
+			end
 		end
 	elsif girdi==2
-		loop do
-        	puts "Cok Yakinda"
-        	sleep(1.0)
-        	system("clear")
-        	sleep(1.0)
-		end
+		
 	
 	elsif girdi==3 
 		break
