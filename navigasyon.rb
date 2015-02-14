817e42de5757273822b9a8781bb55623e5324f1f
ozgurefe <ozgrefe@gmail.com>
2015-02-14 15:14:40 +0200

    istatistik grafiğindeki sorunlar giderildi

--- navigasyon.rb
+++ navigasyon.rb
@@ -18,27 +18,31 @@ using TurkishSupport
 
 	def self.istatistik (istatistik)
 		
-		dosya = File.new('veritabani.txt')
-		satir = dosya.readlines.last
-		dosya.close
-		kota = satir.split(',')[3]
+		File.new('veritabani.txt','r').readlines().each do |satir|
+			parca = satir.split(",")
+			if parca[0] == istatistik
+				@kota = parca[3]
+			else
+				@kota = 0
+			end
+		end
 		
 		@bilgi 	= `du -s #{istatistik}`
 	 	bilgi 	= @bilgi.chomp.split(/\t/)
 		diyez 	= (bilgi[0].to_f/((@kota.to_i)*1024/20)).to_f
 
 		if diyez < 1 
-			kullanim = 1
+			@kullanim = 1
 		else
-			kullanim = diyez
+			@kullanim = diyez
 		end 
-
+		
 		puts "Bulunduğunuz dizin: #{bilgi[1]}"
 		puts "Kullanım miktarı: #{bilgi[0]} K"
 		puts "Kullanım Grafiğiniz:"
-		grafik = "[#{'#'*kullanim.to_i}                   ] #{bilgi[0]}K/#{@kota.to_i}M"
+		grafik = "[#{'#' * @kullanim.to_i}                   ] #{bilgi[0]}K/#{@kota.to_i}M"
 		puts grafik
-			
+		sleep(5.0)	
 	end        
 	
 	def self.listele
